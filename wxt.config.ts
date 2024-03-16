c1287ecb310738ec65802b8fbf70f31110775699
KazooTTT <31075337+KazooTTT@users.noreply.github.com>
2024-03-16 18:28:21 +0800

    feat: update the icons

--- wxt.config.ts
+++ wxt.config.ts
@@ -3,6 +3,9 @@ import react from "@vitejs/plugin-react";
 
 // See https://wxt.dev/api/config.html
 export default defineConfig({
+  manifest: {
+    host_permissions: ["https://github.com/*/*"],
+  },
   vite: () => ({
     plugins: [react()],
   }),

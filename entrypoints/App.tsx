022eb784d9e37d8756eab051fee1b79377ecfdf7
KazooTTT <yjwang.echo@gmail.com>
2024-03-17 14:50:16 +0800

    fix: typo

--- entrypoints/App.tsx
+++ entrypoints/App.tsx
@@ -2,41 +2,45 @@ import React from "react";
 import { useEffect } from "react";
 
 const createButton = (repoLink: string) => {
-  const gitHubBoxLink = repoLink.replace("github.com", "githubbox.com")
+  const gitHubBoxLink = repoLink.replace("github.com", "githubbox.com");
 
   const button = document.createElement("a");
   button.href = gitHubBoxLink;
   button.target = "_blank";
-  button.textContent = "Open on CodeSandBox";
-  button.classList.add("open-sandbox-btn", 'btn-sm', 'btn');
+  button.textContent = "Open in CodeSandBox";
+  button.classList.add("open-sandbox-btn", "btn-sm", "btn");
   return button;
-}
+};
 
-const getTextContent = (element: HTMLElement | null) => element?.textContent?.trim() || '';
+const getTextContent = (element: HTMLElement | null) =>
+  element?.textContent?.trim() || "";
 
 const AddButtonInRepo = ({ currentUrl }: { currentUrl: string }) => {
   useEffect(() => {
-    const repoContainerComponent = document.getElementById('repository-container-header')
-    const repoTitleComponent = document.getElementById('repo-title-component');
+    const repoContainerComponent = document.getElementById(
+      "repository-container-header"
+    );
+    const repoTitleComponent = document.getElementById("repo-title-component");
 
-    const isPublic = getTextContent(repoContainerComponent ?? repoTitleComponent) 
+    const isPublic = getTextContent(
+      repoContainerComponent ?? repoTitleComponent
+    );
 
     if (isPublic) {
-      const button = createButton(currentUrl)
-      const ul = document.getElementById('repository-details-container')?.querySelector('ul');
+      const button = createButton(currentUrl);
+      const ul = document
+        .getElementById("repository-details-container")
+        ?.querySelector("ul");
       ul?.firstChild && ul?.append(button, ul.firstChild);
     }
+  }, []);
 
-  }, [])
-
-
-  return <></>
-}
+  return <></>;
+};
 
 const App = () => {
   const currentUrl = window.location.href;
   return <AddButtonInRepo currentUrl={currentUrl} />;
-}
-
+};
 
 export default App;
